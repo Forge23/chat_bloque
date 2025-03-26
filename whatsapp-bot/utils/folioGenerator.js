b135204365e41bad2ef9611bbf876c85955477d9
Hernández López Angel David <angelxone@outlook.com>
2025-03-26 08:14:20 -0600

    sin_bot2

--- whatsapp-bot/utils/folioGenerator.js
+++
@@ -1,9 +0,0 @@
-function generateFolio() {
-    const timestamp = Date.now().toString();
-    const random = Math.random().toString(36).substring(2, 8).toUpperCase();
-    return `${timestamp.slice(-6)}${random}`;
-}
-
-module.exports = {
-    generateFolio
-};
